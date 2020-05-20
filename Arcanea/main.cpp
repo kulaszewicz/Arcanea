bdb1c3003095e4c4ba64e8af9f2e3fab3ad4da5d
Kuba <s17838@pjwstk.edu.pl>
2020-05-21 01:57:33 +0200

    Added main Game class with basic methods and properties

--- Arcanea/main.cpp
+++ Arcanea/main.cpp
@@ -1,17 +1,20 @@
 #include "SDL.h"
+#include "Game.hpp"
+
+Game* game = nullptr;
 
 int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Arcanea", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
-	SDL_RenderClear(renderer);
 	
-	SDL_RenderPresent(renderer);
+	game = new Game();
 
-	SDL_Delay(3000);
+	game->init("Arcanea", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	game->clean();
 
 	return 0;
 }
